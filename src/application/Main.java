30f5da40c510f796c1ff2ce950f17fe61284d728
Diegx22v <bydiego500@gmail.com>
2024-09-12 08:42:59 -0500

    projecto java fx hecho por: Franklin Chunga y Diego Villota

---
+++ src/application/Main.java
@@ -0,0 +1,30 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+
+public class Main extends Application {
+    @Override
+    public void start(@SuppressWarnings("exports") Stage primaryStage) throws Exception {
+    	
+        Image icono = new Image(getClass().getResourceAsStream("6.png"));
+        primaryStage.getIcons().add(icono);
+        Parent root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
+        Scene scene = new Scene(root, 800, 600);
+        
+
+        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+        
+        primaryStage.setTitle("Proyecto");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module ejem {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.web;
+	requires javafx.graphics;
+	requires java.desktop;
+    opens application to javafx.fxml;
+    exports application;
+}
