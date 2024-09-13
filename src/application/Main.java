fba66a3a1c9634ee10159ccc2041599b1b20babe
Diegx22v <153206791+Diegx22v@users.noreply.github.com>
2024-09-12 22:27:51 -0500

    Clase Main

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.image.Image;
+import javafx.scene.image.Image; 
 
 public class Main extends Application {
     @Override
