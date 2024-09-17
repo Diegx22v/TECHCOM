9c34c23e539da76d755824c78348ef551257fc8a
Diegx22v <bydiego500@gmail.com>
2024-09-17 10:18:00 -0500

    D: nuevo paquete llamado utils para tener funciones que se vayan a llamar varias veces  y deje el import listo en el Main.java para algun uso o metodo agregado.

---
+++ src/utils/Utils.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package utils;
+
+/**
+ * 
+ */
+public class Utils {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// principal
+	}
+	// crear metodos de java fx para limitar la cantidad de lineas y generar codigo mas legible 
+}
