c3e3e4e2608d9e88facd7f18e8a64abf99639a8f
Diegx22v <bydiego500@gmail.com>
2024-09-21 18:54:25 -0500

    D: metodos acomodados a espera de mejores soluciones.

--- src/application/Controlador.java
+++ src/application/Controlador.java
@@ -41,8 +41,8 @@ public class Controlador {
     
     @SuppressWarnings("unused")
     private ScaleTransition animacionTechcom;
-	private ScaleTransition animacion_dos,animacion_uno,animacion_tres,animacion_cuatro,animacion_cinco,animacion_click1,animacion_click2;
-    
+	private ScaleTransition animacion_dos,animacion_uno,animacion_tres,animacion_cuatro,animacion_cinco;
+    private ScaleTransition animacion_click1,animacion_click2,animacion_click3,animacion_click4,animacion_click5;
     @FXML 
     private Button creditos;
     @FXML 
@@ -65,26 +65,15 @@ public class Controlador {
     	    inicializarAnimacion();
     	    inicializarAnimacion_boton_video1();
     	    inicializarAnimacion_boton_video2();
-    	    inicializarAnimacion_click_video2();
     	    inicializarAnimacion_boton_jugar();
     	    inicializarAnimacion_boton_creditos();
     	    inicializarAnimacion_boton_salir();
     	    inicializarAnimacion_click_video1();
+    	    inicializarAnimacion_click_video2();
+    	    inicializarAnimacion_click_jugar();
+    	    inicializarAnimacion_click_salir();
     }
     
-private void inicializarAnimacion_click_video1() {
-        
-	animacion_click1 = new ScaleTransition(Duration.millis(150), video_uno);
-	animacion_click1.setByX(0.15);  
-	animacion_click1.setByY(0.10);  
-	animacion_click1.setAutoReverse(true);
-	animacion_click1.setCycleCount(2);
-    }
-    
-    void animacion_click_boton1(ScaleTransition animacion_click1) {
-        if (animacion_click1.getStatus() != Status.RUNNING) {
-        	animacion_click1.playFromStart(); 
-        }}
     
     private void inicializarAnimacion_boton_video1() {
         
@@ -94,6 +83,10 @@ private void inicializarAnimacion_click_video1() {
         animacion_uno.setAutoReverse(true);
         animacion_uno.setCycleCount(2);
     }
+    void animacion_soft_boton1(ScaleTransition animacion_uno) {
+        if (animacion_uno.getStatus() != Status.RUNNING) {
+            animacion_uno.playFromStart(); 
+        }}
     
  private void inicializarAnimacion_boton_video2() {
         
@@ -103,11 +96,12 @@ private void inicializarAnimacion_click_video1() {
         animacion_dos.setAutoReverse(true);
         animacion_dos.setCycleCount(2);
     }
+ void animacion_soft_boton2(ScaleTransition animacion_dos) {
+     if (animacion_dos.getStatus() != Status.RUNNING) {
+     	animacion_dos.playFromStart(); 
+     }}
     
-    void animacion_soft_boton1(ScaleTransition animacion_uno) {
-        if (animacion_uno.getStatus() != Status.RUNNING) {
-            animacion_uno.playFromStart(); 
-        }}
+
     
 private void inicializarAnimacion_boton_jugar() {
         
@@ -125,33 +119,45 @@ private void inicializarAnimacion_boton_jugar() {
     
     private void inicializarAnimacion_boton_creditos() {
         
-    	animacion_cinco = new ScaleTransition(Duration.millis(150), creditos);
-    	animacion_cinco.setByX(0.15);  
-    	animacion_cinco.setByY(0.10);  
-    	animacion_cinco.setAutoReverse(true);
-    	animacion_cinco.setCycleCount(2);
+    	animacion_cuatro = new ScaleTransition(Duration.millis(150), creditos);
+    	animacion_cuatro.setByX(0.15);  
+    	animacion_cuatro.setByY(0.10);  
+    	animacion_cuatro.setAutoReverse(true);
+    	animacion_cuatro.setCycleCount(2);
         }
         
-    void animacion_soft_creditos(ScaleTransition animacion_cinco) {
-        if (animacion_cinco.getStatus() != Status.RUNNING) {
-        	animacion_cinco.playFromStart(); 
+    void animacion_soft_creditos(ScaleTransition animacion_cuatro) {
+        if (animacion_cuatro.getStatus() != Status.RUNNING) {
+        	animacion_cuatro.playFromStart(); 
         }}
         
         private void inicializarAnimacion_boton_salir() {
             
-        	animacion_cuatro = new ScaleTransition(Duration.millis(150), salir);
-        	animacion_cuatro.setByX(0.15);  
-        	animacion_cuatro.setByY(0.10);  
-        	animacion_cuatro.setAutoReverse(true);
-        	animacion_cuatro.setCycleCount(2);
+        	animacion_cinco = new ScaleTransition(Duration.millis(150), salir);
+        	animacion_cinco.setByX(0.15);  
+        	animacion_cinco.setByY(0.10);  
+        	animacion_cinco.setAutoReverse(true);
+        	animacion_cinco.setCycleCount(2);
             }
             
-        void animacion_soft_salir(ScaleTransition animacion_cuatro) {
-            if (animacion_cuatro.getStatus() != Status.RUNNING) {
-            	animacion_cuatro.playFromStart(); 
+        void animacion_soft_salir(ScaleTransition animacion_cinco) {
+            if (animacion_cinco.getStatus() != Status.RUNNING) {
+            	animacion_cinco.playFromStart(); 
             }}
         
+        private void inicializarAnimacion_click_video1() {
             
+        	animacion_click1 = new ScaleTransition(Duration.millis(150), video_uno);
+        	animacion_click1.setByX(0.15);  
+        	animacion_click1.setByY(0.10);  
+        	animacion_click1.setAutoReverse(true);
+        	animacion_click1.setCycleCount(2);
+            }
+            
+            void animacion_click_boton1(ScaleTransition animacion_click1) {
+                if (animacion_click1.getStatus() != Status.RUNNING) {
+                	animacion_click1.playFromStart(); 
+                }}
     
  private void inicializarAnimacion_click_video2() {
         
@@ -166,15 +172,32 @@ private void inicializarAnimacion_boton_jugar() {
     	 animacion_click2.playFromStart(); 
      }}
     
-    void animacion_soft_boton2(ScaleTransition animacion_dos) {
-        if (animacion_dos.getStatus() != Status.RUNNING) {
-        	animacion_dos.playFromStart(); 
-        }}
-    
-    @FXML
-    void proyecto_web_2(ActionEvent event)throws URISyntaxException,IOException {
-    	Desktop.getDesktop().browse(new URI("https://elevenlabs.io/"));
+ private void inicializarAnimacion_click_jugar() {
+     
+	 animacion_click3 = new ScaleTransition(Duration.millis(200), jugar);
+	 animacion_click3.setByX(0.20);  
+	 animacion_click3.setByY(0.20);  
+	 animacion_click3.setAutoReverse(true);
+	 animacion_click3.setCycleCount(2);
     }
+ void animacion_click_boton3(ScaleTransition animacion_click3) {
+     if (animacion_click3.getStatus() != Status.RUNNING) {
+    	 animacion_click3.playFromStart(); 
+     }}
+// falta el click de los creditos y el valor de salir a 5 (4 pertenece a creditos)
+ 
+ private void inicializarAnimacion_click_salir() {
+     
+	 animacion_click4 = new ScaleTransition(Duration.millis(200), salir);
+	 animacion_click4.setByX(0.20);  
+	 animacion_click4.setByY(0.20);  
+	 animacion_click4.setAutoReverse(true);
+	 animacion_click4.setCycleCount(2);
+    }
+ void animacion_click_boton4(ScaleTransition animacion_click4) {
+     if (animacion_click4.getStatus() != Status.RUNNING) {
+    	 animacion_click4.playFromStart(); 
+     }}
     
     @FXML
     void  softcursor1() {
@@ -193,14 +216,42 @@ private void inicializarAnimacion_boton_jugar() {
     
     @FXML
     void softcursor4() {
-    	animacion_soft_creditos(animacion_cinco);
+    	animacion_soft_creditos(animacion_cuatro);
     }
     
     @FXML
     void softcursor5() {
-    	animacion_soft_salir(animacion_cuatro);
+    	animacion_soft_salir(animacion_cinco);
     }
     
+    @FXML
+    void set_video1() {
+    	animacion_click_boton1(animacion_click1);
+
+    	cargar_video("https://www.youtube.com/embed/kAbrnfx7axc");
+    }
+    
+    @FXML
+    void set_video2() {
+    	animacion_click_boton2(animacion_click2);
+    	cargar_video("https://www.youtube.com/embed/7kcF9v4oo7M");
+    }
+    
+    @FXML
+    private void iniciarJuego() {
+    	animacion_click_boton3(animacion_click3);
+    	try { Utils.juego_laberinto(); Stage stage = (Stage) salir.getScene().getWindow();  stage.close(); }
+    	catch(Exception e) { alerta_de_error(e);  }
+    }   
+    
+    @FXML
+    public void cerrarVentana() {
+    	animacion_click_boton4(animacion_click4);
+        Stage stage = (Stage) salir.getScene().getWindow();
+        stage.close();
+    }
+    
+    //imagen
     private void inicializarAnimacion() {
         
         animacionTechcom = new ScaleTransition(Duration.millis(150), Techcom);
@@ -221,29 +272,14 @@ private void inicializarAnimacion_boton_jugar() {
         animacion_image(animacionTechcom);
     }
 
-    
-    @FXML
-    public void cerrarVentana() {
-        Stage stage = (Stage) salir.getScene().getWindow();
-        stage.close();
-    }
-    
-    @FXML
-    void set_video1() {
-    	animacion_click_boton1(animacion_click1);
-
-    	cargar_video("https://www.youtube.com/embed/kAbrnfx7axc");
-    }
-    
-    @FXML
-    void set_video2() {
-    	animacion_click_boton2(animacion_click2);
-    	cargar_video("https://www.youtube.com/embed/7kcF9v4oo7M");
-    }
-    
     @FXML
     void proyecto_web(ActionEvent event) throws URISyntaxException,IOException{
     	Desktop.getDesktop().browse(new URI("https://colab.research.google.com/drive/1dygH6NjFvrz2Slhufk2ilNR_kD5C48as"));
+    }
+    
+    @FXML
+    void proyecto_web_2(ActionEvent event)throws URISyntaxException,IOException {
+    	Desktop.getDesktop().browse(new URI("https://elevenlabs.io/"));
     }
     
     public void cargar_video(String url) {
@@ -293,12 +329,6 @@ private void inicializarAnimacion_boton_jugar() {
     public boolean getjuegoiniciado() {
     	return juego_iniciado;
     }
-    
-    @FXML
-    private void iniciarJuego() {
-    	try { Utils.juego_laberinto();  }
-    	catch(Exception e) { alerta_de_error(e);  }
-    }   
 
     
 }
